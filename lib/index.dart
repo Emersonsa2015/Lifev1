f71cd0fdf6e4e19d95f9905a2900a17838aa8c36
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-03-06 13:00:20 +0000

    Updating to latest FlutterFlow output.

---
+++ lib/index.dart
@@ -0,0 +1,5 @@
+// Export pages
+export '/landing_page/landing_page_widget.dart' show LandingPageWidget;
+export '/year/year_widget.dart' show YearWidget;
+export '/principal/principal_widget.dart' show PrincipalWidget;
+export '/home0/home0_widget.dart' show Home0Widget;
